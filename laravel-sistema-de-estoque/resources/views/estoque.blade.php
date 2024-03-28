b0ac52d58c75f0f139b7d99939fc97a70adf0e3b
LUCAS VIANA DA SILVA <lucas.silva134@portalsesisp.org.br>
2024-03-28 16:09:36 -0300

    Laravel Adicionado

---
+++ laravel-sistema-de-estoque/resources/views/estoque.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <header>
+        <h1>Seja bem-vindo à página inicial!</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/front">Front</a></li>
+        </ul>
+    </nav>
+    <main>
+        <p>Aqui você pode adicionar o conteúdo da sua página inicial.</p>
+    </main>
+    <footer>
+        <p>Rodapé da página inicial</p>
+    </footer>
+</body>
+</html>
